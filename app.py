1da829c0ea575e5a8b401b1871af5d714d8fb8b1
Katsushi <willbefree.k.m@gmail.com>
2024-10-17 16:17:07 +0900

    add text elements to the app

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
 import streamlit as st
 
 st.title("my streamlit app")
+st.header('lesson 3 : add texts')
 
-st.write("Hello streamlit")
+st.write("normal text")
+st.markdown("this is **bold** and this is *italic*")
+st.latex(r'E=mc^2')
+st.latex(r'\sqrt{x^2 + y^2} = z')
+
+st.info('info')
+st.warning('warning')
+st.error('error')
+st.success('success')
+
+code = '''
+def hello():
+    print("hello streamlit")
+'''
+st.code(code, language='python')
